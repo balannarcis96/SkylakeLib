5a4cedb83c8c8728d6b02f611c653c9229a24f1f
balannarcis <narcis.balan@deventure.com>
2022-11-21 20:00:14 +0200

    Fixed UIDStore small bug

--- SkylakeLibStandalone/ECS/UIDStore.h
+++ SkylakeLibStandalone/ECS/UIDStore.h
@@ -66,7 +66,7 @@ namespace SKL
         {
             TUIDType Result{ IdentityValue };
 
-            if( static_cast<TUIDType>( AllocationsCount.increment() + 1 ) < MaxUIDValue ) SKL_LIKELY
+            if( static_cast<TUIDType>( AllocationsCount.increment() + 1 ) <= MaxUIDValue ) SKL_LIKELY
             {
                 if( TRUE == bIsActive ) SKL_LIKELY
                 {
